f74dd77ba09502931cfc44aabbb0268f0f2d31b9
invitado <invitado@s5pc42.eisc.univalle.edu.co>
2019-06-13 11:55:41 -0500

    adicionando fork-wait.c

---
+++ 2019_06_13/fork-wait.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char**argv){
+   pid_t pid,pid2;
+   int status;
+  
+   pid = fork();
+   if (pid != 0){
+     pid2 = wait(&status);
+     printf("soy el padre con id %d y el de mi hijo es %d\n",getpid(),pid);
+  } else{
+    sleep(3);
+    printf("soy el hijo con id %d y mi variable pid es %d\n",getpid(),pid);
+  }
+return 0;
+}
